d2d4700eb829ed00c958c633950d4787d6903a81
Kayla Hood <kaylahood1996@gmail.com>
2022-11-11 13:40:45 -0600

    Initial commit. Tested on Python 3.9 and Faker 15.1.1

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # faker-stubs
-Quick and dirty faker stubs for Faker 15.1.1
+Quick and dirty script to generate faker stubs for Faker 15.1.1
+
+# Prerequisites
+- You must have Faker installed *(this has been tested with Faker 15.1.1 but it probably works for other versions as well)*
+- Your Python version must be 3.9 *(I have not tested this script on any other version)*
+
+# To install
+This isn't hosted on pypi or any other python package sharing site, so you'll need to install this manually if you're really determined to get autocompletion for Faker.
+Luckily, the installation is pretty straightforward. Here are the steps:
+1. Read the Prerequisites section and make sure your environment satisfies all of them.
+2. Download generate_stub.py and save it anywhere on your machine. If you ever update the faker library you'll need to run it again, so keep it somewhere you'll remember.
+3. Run the file *(you can do this however you'd like, but I've included the steps for the most common method below)*
+    1. For unix/macOS: Open a terminal session and run the following commands
+        ```
+        cd ${directory_where_you_saved_generate_stub}
+        python ./generate_stub.py
+        ```  
+        (`${directory_where_you_saved_generate_stub}` *should be replaced with the POSIX-style path to the directory where you saved generate_stub.py)*
+    2. For windows: Open a command prompt session and run the following commands
+        ```
+        cd %DIRECTORY_WHERE_YOU_SAVED_GENERATE_STUB%
+        python .\generate_stub.py
+        ```  
+        (`%DIRECTORY_WHERE_YOU_SAVED_GENERATE_STUB%` *should be replaced with the Windows-style path to the directory where you saved generate_stub.py)*
+4. Open your favorite IDE and try auto-completion on a Faker instance, all of the provider methods (for all locales) should be included in the drop-down!
+
+# Notes
+- ALL locales' provider methods (including the default locale) are included in the stub. This means that some of the methods in your auto-completion might not actually be present in your Faker instance if you selected a specific locale which doesn't contain the method you want to use.  
+    If you create your faker instance using the normal initialization method with no locale arguments (i.e. `faker.Faker()`) then you don't need to worry about this; all of the methods in the auto-completion dropdown will be available to your instance.
---
+++ generate_stub.py
@@ -0,0 +1,96 @@
+from collections import defaultdict
+import inspect
+import pathlib
+import re
+from typing import Tuple
+from faker.config import AVAILABLE_LOCALES, PROVIDERS
+from faker import Factory
+
+import faker.proxy
+
+
+def get_module_and_member(cls) -> Tuple[str, str]:
+    cls_name = getattr(cls, '__name__', getattr(cls, '_name', str(cls)))
+    module, member = cls.__module__, cls_name
+    if cls_name is None:
+        qualified_types = re.findall(r'([a-zA-Z_0-9]+)\.([a-zA-Z_0-9]+)', str(cls))
+        for module, member_name in qualified_types:
+            if member_name not in imports[module]:
+                module, member = module, member_name
+        unqualified_types = re.findall(r'[^\.a-zA-Z0-9_]([A-Z][a-zA-Z0-9_]+)[^\.a-zA-Z0-9_]', ' ' + str(cls) + ' ')
+        for type_name in unqualified_types:
+            if type_name == "NoneType":
+                continue
+            cls_str = str(prov_cls).replace('.en_US', '').replace('.'+locale, '').split("'")[1].replace("faker.", ".")
+            if type_name not in imports[cls_str]:
+                module, member = cls_str, type_name
+    return module, member
+    
+
+unique_members = {}
+for locale in AVAILABLE_LOCALES:
+    for provider in PROVIDERS:
+        if provider == "faker.providers":
+            continue
+        prov_cls, _, _ = Factory._find_provider_class(provider, locale)
+        members = {name: (prov_cls, locale, value) for (name, value) in inspect.getmembers(prov_cls) if not name.startswith("_")}
+        unique_members.update(members)
+
+imports = defaultdict(set)
+imports["typing"] = {"TypeVar"}
+imports["enum"] = {"Enum"}
+signatures = []
+for name, (prov_cls, locale, value) in unique_members.items():
+    attr = getattr(prov_cls, name, None)
+    if attr is not None and inspect.isfunction(attr) or inspect.ismethod(attr):
+        sig = inspect.signature(value)
+        if sig.return_annotation is not inspect.Signature.empty and sig.return_annotation.__module__ != "builtins":
+            module, member = get_module_and_member(sig.return_annotation)
+            if module is not None and member is not None:
+                imports[module].add(member)
+        new_parms = []
+        for key, parm_val in sig.parameters.items():
+            new_parm = parm_val
+            if parm_val.default is not inspect.Parameter.empty:
+                new_parm = parm_val.replace(default=...)
+            if new_parm.annotation is not inspect.Parameter.empty and new_parm.annotation.__module__ != "builtins":
+                module, member = get_module_and_member(new_parm.annotation)
+                if module is not None and member is not None:
+                    imports[module].add(member)
+            new_parms.append(new_parm)
+        sig = sig.replace(parameters=new_parms)
+        sig_str = str(sig).replace("Ellipsis", "...").replace("NoneType", "None").replace("~", "")
+        for module in imports.keys():
+            sig_str = sig_str.replace(f"{module}.", "")
+        signatures.append(f"def {name}{sig_str}: ...")
+    else:
+        new_modules = []
+        type_module = getattr(type(value), "__module__", None)
+        if type_module is not None and type_module != "builtins":
+            module, member = get_module_and_member(type(value))
+            if module is not None and member is not None:
+                imports[module].add(member)
+                new_modules.append(module)
+        type_str = type(value).__name__.replace("Ellipsis", "...").replace("NoneType", "None").replace("~", "")
+        for module in new_modules:
+            type_str = type_str.replace(f"{module}.", "")
+        signatures.append(f"{name}: {type_str}")
+
+imports_block = "\n".join([f"from {module} import {', '.join(names)}" for module, names in imports.items()])
+member_signatures_block = "    " + "\n    ".join(signatures)
+
+body = \
+f"""
+{imports_block}
+
+T = TypeVar(\"T\")
+TEnum = TypeVar("TEnum", bound=Enum)
+
+class Faker:
+{member_signatures_block}
+"""
+
+faker_proxy_path = pathlib.Path(inspect.getfile(faker.proxy))
+stub_file_path = faker_proxy_path.with_name("proxy.pyi").resolve()
+with open(stub_file_path, "w") as fh:
+    fh.write(body)
